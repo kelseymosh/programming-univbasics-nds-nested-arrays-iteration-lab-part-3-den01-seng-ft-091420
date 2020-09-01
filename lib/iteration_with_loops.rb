b7aadf2d22ffa38bcf952cca8ba68af3aab0a53e
Kelsey Oshiro <kelseymosh@gmail.com>
2020-09-01 18:44:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,9 @@
 def join_nested_strings(src)
+  mixed_data_1 = [
+    ["The", 4, "quick"],
+    [-1, "brown", "fox", 30],
+    ["studied", 101, 233, "Ruby"]
+  ]
   new_array = []
   total = 0
   row_index = 0

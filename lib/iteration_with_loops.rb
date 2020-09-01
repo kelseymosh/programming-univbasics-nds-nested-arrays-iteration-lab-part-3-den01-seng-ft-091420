5a0e18d4b39c5770ba5ffb0008efb11d49c02708
Kelsey Oshiro <kelseymosh@gmail.com>
2020-09-01 18:44:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      if mixed_data_1.class == 'string'
+      if mixed_data_1.class == String
       new_array << mixed_data_1
       element_index += 1
       end
